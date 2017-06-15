10fa5efd956704a05ebbcc974fcd6235baa56a71
Hugo Morales <hmoralesg@icloud.com>
2017-06-15 19:54:25 -0400

    1 - Ejercicio 2

--- 1 metodos/ejercicio2.rb
+++ 1 metodos/ejercicio2.rb
@@ -1,15 +1,8 @@
-# El siguiente programa debería mostrar sí o no, sin embargo muestrar error
+# El siguiente programa deberia mostrar si o no, sin embargo muestrar error
 # Se pide identificar el error y corregirlo.
 
 def random
-  result = [true, false].sample
-  puts result
+  [true, false].sample
 end
 
-if random == true
-  puts 'sí'
-elsif random == false
-  puts 'no'
-else
-  puts 'error'
-end
+puts random ? 'si' : 'no'
