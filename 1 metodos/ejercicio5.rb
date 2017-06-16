a1cb6e907479a32a6cf3c352b8fa08c4d0424689
Hugo Morales <hmoralesg@icloud.com>
2017-06-15 20:07:54 -0400

    1 - Ejercicio 5

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,8 @@
-# Crear un método que reciba como parámetro dos números enteros positivos
-# e imprima los números pares que existen entre esos dos números.
+# Crear un metodo que reciba como parametro dos numeros enteros positivos
+# e imprima los numeros pares que existen entre esos dos numeros.
+
+def dos_numeros(a, b)
+  (a..b).each { |i| puts i if (i % 2).zero? }
+end
+
+dos_numeros(2, 16)
